57f94e0525b94fa9956df46cb3a84af49b0e5628
Jonas Couto <jonas-couto@hotmail.com>
2023-02-28 21:54:17 -0300

    Creat new Project of Test in WebSite

---
+++ cypress/e2e/SwagLabs.cy.js
@@ -0,0 +1,16 @@
+describe('template spec', () => {
+
+  beforeEach(() => {
+    cy.visit(Cypress.config('baseUrl'),{ timeout: 60000 })
+  });
+
+
+
+  it('Testando Adicionar produtos no carrinho', () => {
+  
+    //cy.get(':nth-child(1) > .pricebar > .btn_primary').click().should('be.visible')
+    //cy.get('#shopping_cart_container').click()
+
+  
+  })
+})
